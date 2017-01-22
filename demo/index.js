3fe900197d2b3fb70362529452501fcfec4c60e7
Антон Васин <mail@vasin.space>
2017-01-22 16:15:15 +0300

    Matrices support

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import Context from '../lib/Context'
+import Matrix from '../lib/Matrix'
 
-let ctx = new Context(document.getElementById('glcanvas'));
+let canvas = document.getElementById('glcanvas');
+
+let ctx = new Context(canvas);
 
 let vertexShaderPromise = ctx.createShader('./shaders/vertex.glsl', ctx.gl.VERTEX_SHADER);
 let fragmentShaderPromise = ctx.createShader('./shaders/fragment.glsl', ctx.gl.FRAGMENT_SHADER);
@@ -36,6 +39,10 @@ programPromise.then(program => {
 
     program.use();
 
+    gl.uniformMatrix4fv(program.getUniformLocation("projection"), false,
+        new Float32Array(Matrix.perspective(60, canvas.width/ canvas.height, 0.1, 100.0).toArray()));
+    gl.uniformMatrix4fv(program.getUniformLocation("modelView"), false, Matrix.translate(Matrix.vector([0, 0, -5])).toArray());
+
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
     gl.vertexAttribPointer(program.getAttributeLocation('vPosition'), 3, gl.FLOAT, false, 0, 0);
@@ -49,7 +56,7 @@ programPromise.then(program => {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
